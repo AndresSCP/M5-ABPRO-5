b8fd1ab7141b5df7ec85fe4017c643c6fea3bc33
AndresSCP <andres.contreras.p@gmail.com>
2023-04-05 21:48:48 -0400

    clase, impl, dao profesional

--- src/main/java/modelo/Profesional.java
+++ src/main/java/modelo/Profesional.java
@@ -1,58 +1,59 @@
 package modelo;
 
+import java.util.Date;
+
 public class Profesional extends Usuario {
-    private int telefono;
+    private Integer run;
     private String tituloProfesional;
+    private Date FechaIngreso;
     private String proyecto;
+	public Profesional() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Profesional(Integer run,String tituloProfesional, Date fechaIngreso, String proyecto, String nombre, Date fechaNac, Integer tipo) {
+		super(run, nombre, fechaNac, tipo);
+		// TODO Auto-generated constructor stub
+		this.run = run;
+		this.tituloProfesional = tituloProfesional;
+		this.FechaIngreso = fechaIngreso;
+		this.proyecto = proyecto;
+		
+	}
+	public Integer getRun() {
+		return run;
+	}
+	public void setRun(int run) {
+		this.run = run;
+	}
+	public String getTituloProfesional() {
+		return tituloProfesional;
+	}
+	public void setTituloProfesional(String tituloProfesional) {
+		this.tituloProfesional = tituloProfesional;
+	}
+	public Date getFechaIngreso() {
+		return FechaIngreso;
+	}
+	public void setFechaIngreso(Date fechaIngreso) {
+		FechaIngreso = fechaIngreso;
+	}
+	public String getProyecto() {
+		return proyecto;
+	}
+	public void setProyecto(String proyecto) {
+		this.proyecto = proyecto;
+	}
+	@Override
+	public String toString() {
+		return "Profesional [run=" + run + ", tituloProfesional=" + tituloProfesional + ", FechaIngreso=" + FechaIngreso
+				+ ", proyecto=" + proyecto + ", getRun()=" + getRun() + ", getTituloProfesional()="
+				+ getTituloProfesional() + ", getFechaIngreso()=" + getFechaIngreso() + ", getProyecto()="
+				+ getProyecto() + ", getNombre()=" + getNombre() + ", getFechaNac()=" + getFechaNac() + ", getTipo()="
+				+ getTipo() + ", toString()=" + super.toString() + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + "]";
+	}
+
     
-    public Profesional(int id, int runUsuario, String nombreUsuario, String apellidoUsuario, int idUsuario, 
-                       String fechaNacimientoUsuario, int telefono, String tituloProfesional, String proyecto) {
-        super(id, runUsuario, nombreUsuario, apellidoUsuario, fechaNacimientoUsuario, idUsuario);
-        this.telefono = telefono;
-        this.tituloProfesional = tituloProfesional;
-        this.proyecto = proyecto;
-    }
     
-    public Profesional() {
-        super();
-    }
-
-    public int getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(int telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getTituloProfesional() {
-        return tituloProfesional;
-    }
-
-    public void setTituloProfesional(String tituloProfesional) {
-        this.tituloProfesional = tituloProfesional;
-    }
-
-    public String getProyecto() {
-        return proyecto;
-    }
-
-    public void setProyecto(String proyecto) {
-        this.proyecto = proyecto;
-    }
-
-    @Override
-    public String toString() {
-        return "Profesional{" +
-                "id=" + this.getId() +
-                ", runUsuario=" + this.getRunUsuario() +
-                ", nombreUsuario='" + this.getNombreUsuario() + '\'' +
-                ", apellidoUsuario='" + this.getApellidoUsuario() + '\'' +
-                ", fechaNacimientoUsuario='" + this.getFechaNacimientoUsuario() + '\'' +
-                ", idUsuario=" + this.getIdUsuario() +
-                ", telefono=" + telefono +
-                ", tituloProfesional='" + tituloProfesional + '\'' +
-                ", proyecto='" + proyecto + '\'' +
-                '}';
-    }
 }
